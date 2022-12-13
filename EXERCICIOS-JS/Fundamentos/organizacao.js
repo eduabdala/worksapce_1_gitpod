69c435116483973fda0ed47957be422763a8c36d
Eduardo Abdala <eduardosilvaabdala8@gmail.com>
2022-12-13 03:50:02 +0000

    new file: EXERCICIOS-JS/Fundamentos/organizacao.js

---
+++ EXERCICIOS-JS/Fundamentos/organizacao.js
@@ -0,0 +1,13 @@
+console.log("sentencas de codigo");
+
+{
+
+    {
+        console.log("ola");
+        console.log("mundo!") //padrao do curso
+    }
+
+}
+
+// js e organizado em sentencas de codigos que podem ou nao ser finalizados por ;
+// e tbm e organizado em blocos de codigos, esse blocos sao determidados por {}
