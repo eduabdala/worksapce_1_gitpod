b96467ca555905f8fa16e787d73f923f68e3749c
Eduardo Abdala <eduardosilvaabdala8@gmail.com>
2022-12-13 20:36:23 +0000

    new file

---
+++ EXERCICIOS-JS/Fundamentos/Fundamentos/numero.js
@@ -0,0 +1,28 @@
+const peso1 = 1.0
+const peso2 = Number('2.0')
+
+console.log(peso1, peso2)
+console.log(Number.isInteger(peso1))
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total / (peso1 + peso2)
+
+
+console.log(media.toFixed(2))
+console.log(media.toString(2)) // em binario
+console.log(typeof media)
+console.log(typeof Number)
+
+//======================================================//
+
+// Alguns Cuidados
+
+console.log(7 / 0)
+console.log("10" / 2)
+console.log("show" * 2)
+console.log (0.1 + 0.7)
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
---
+++ EXERCICIOS-JS/Fundamentos/Fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = "legal"
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516
+console.log(qualquer);
+console.log(typeof qualquer)
+
+//evitar nome generio e siglas
+let valor = ''
+let numero = ''
+let pqp = false // Produto Quimico Perigoso... kk
+
