ef0fabbbbb87e6e99f732c43f8e0fb3f5cf58645
Eduardo Abdala <eduardosilvaabdala8@gmail.com>
2022-12-13 19:17:01 +0000

    new file

---
+++ EXERCICIOS-JS/Fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+// Comentario de uma linha
+console.log('linha1')
+
+/*
+ Comentario de 
+ multiplas linhas
+*/
+console.log('linha 2')
+
+/*
+* Comentario de
+* multiplas linhas
+*/
+console.log('linha 3')
