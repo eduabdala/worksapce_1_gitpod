6c0779b72af9228fa6ae45be5c360c7f006aeac7
Eduardo Abdala <eduardosilvaabdala8@gmail.com>
2022-12-16 00:58:03 +0000

    new file

---
+++ EXERCICIOS-JS/Fundamentos/Fundamentos/objeto.js
@@ -0,0 +1,8 @@
+// um par de {} repreenta um objeto, 
+// assim como um par de [] representa uma array 
+ const prod1 = {}
+ prod1.nome = 'Celular Ultra Mega'
+ prod1.preco = 4998.90
+
+ console.log(prod1)
+ 
